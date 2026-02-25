986e92287351aec97482089279d3631f764d403b
evrakisleri <evrakisleri83@gmail.com>
2026-02-26 01:43:08 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,11 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-// Frankfurt API Bağlantı Bilgilerin
-// Dikkat: URL'nin başında https:// olduğundan ve sonunda boşluk kalmadığından emin ol.
-const supabaseUrl ='yicxkbmotvnnlwrvubpb'
+const supabaseUrl = 'https://yicxbmotvnnlwrvubpb.supabase.co'
+const supabaseKey = 'sb_publishable_k-RSWSryqGfs9231nHNGIQ_uIa4z6zh' // Frankfurt anon key
 
-// Senin Anon Public Key'in (Panelden aldığımız o uzun kod)
-const supabaseKey ='sb_publishable_k-RSWSryqGfs9231nHNGIQ_uIa4z6zh'
-
-// Proje genelinde kullanılacak olan ana bağlantı objesi
 export const supabase = createClient(supabaseUrl, supabaseKey)

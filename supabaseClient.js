30485dcbf516f7d0f2fdeac43948547a874cf306
evrakisleri <evrakisleri83@gmail.com>
2026-02-26 02:46:29 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://yicxbmotvnnlwrvubpb.supabase.co'
-const supabaseKey = 'sb_publishable_k-RSWSryqGfs9231nHNGIQ_uIa4z6zh' // Frankfurt anon key
+// Frankfurt API - YENİ PROJE BAĞLANTISI
+const supabaseUrl = 'https://ocigkkawnfswgtbbehvn.supabase.co'
+const supabaseKey = 'sb_publishable_-EGX6faLebUu55Dnb7p3Mg_rW2R9gFC'
 
+// Proje genelinde kullanılacak ana bağlantı
 export const supabase = createClient(supabaseUrl, supabaseKey)

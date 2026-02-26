9a1782428967006f63cec3eefae9248f79de5de9
evrakisleri <evrakisleri83@gmail.com>
2026-02-26 03:16:15 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-// Frankfurt Yeni Üs Bağlantısı
 const supabaseUrl = 'https://ocigkkawnfswgtbbehvn.supabase.co'
 const supabaseKey = 'sb_publishable_-EGX6faLebUu55Dnb7p3Mg_rW2R9gFC'
 

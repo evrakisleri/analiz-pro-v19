fa70484b9d808d1aafc6dd1f9353de56e2dc3923
evrakisleri <evrakisleri83@gmail.com>
2026-03-01 18:24:20 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabaseUrl = 'https://ocigkkawnfswgtbbehvn.supabase.co'
-const supabaseKey = 'sb_publishable_-EGX6faLebUu55Dnb7p3Mg_rW2R9gFC'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jaWdra2F3bmZzd2d0YmJlaHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNjI4MDksImV4cCI6MjA4NzYzODgwOX0.nXUrnB7SZHFZ0YQ5fUQTsbIgGxQ0xAr_Gk1aODd-du0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)

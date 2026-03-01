f776321b5396beefb0ce60e9be1846c84c745454
evrakisleri <evrakisleri83@gmail.com>
2026-03-01 23:02:15 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,7 @@
-// supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Kendi Supabase bilgilerini buraya yaz
-const supabaseUrl = 'https://ocigkkawnfswgtbbehvn';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jaWdra2F3bmZzd2d0YmJlaHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNjI4MDksImV4cCI6MjA4NzYzODgwOX0.nXUrnB7SZHFZ0YQ5fUQTsbIgGxQ0xAr_Gk1aODd-du0'; 
+// Kendi bilgilerini buraya gir
+const supabaseUrl = 'https://krhxjjmfzfiraemloksl';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyaHhqam1memZpcmFlbWxva3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTA4OTUsImV4cCI6MjA4Nzk2Njg5NX0.TotlpNCTkt6poP1_vWk1UIDlQlfY58ca25hvzn36VbY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

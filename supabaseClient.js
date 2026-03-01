69279617e9036f7ad3da4283be8bcb0e0e2b670a
evrakisleri <evrakisleri83@gmail.com>
2026-03-02 01:01:09 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const supabaseUrl = "https://sjwndltsoscueesxwkny.supabase.co";
-// Kanka, buraya mutlaka anahtarını tırnak içinde yapıştır!
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqd25kbHRzb3NjdWVlc3h3a255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTI5OTUsImV4cCI6MjA4Nzk2ODk5NX0.bu6kjNfLj_5Y7_Aq__KxTAQjmMS71M3fw_9GFVJ-wDk"; 
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqd25kbHRzb3NjdWVlc3h3a255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTI5OTUsImV4cCI6MjA4Nzk2ODk5NX0.bu6kjNfLj_5Y7_Aq__KxTAQjmMS71M3fw_9GFVJ-wDk"; // Kopyaladığın key'i yapıştır
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
